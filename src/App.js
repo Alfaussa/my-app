5b66cdddc7f85247b4614df8ede081eb61503c31
Alfiya <al.yausheva@gmail.com>
2025-02-05 22:54:11 +0300

    sd

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
 
+
+const prods = [
+  {name: 'product1', cost: 100, id: 1},
+  {name: 'product2', cost: 200, id: 2},
+  {name: 'product3', cost: 300, id: 3},
+ ];
+
 function App() {
- const arr = ['a', 'b', 'c', 'd', 'e'];
-const res = arr.map(function(elem, index){
-  return <li key = {index}>{elem}</li>
+const res = prods.map(function(item){
+  return <p key = {item.id}>
+    {item.name}
+    <span>{item.cost}</span>
+    </p>
 })
 
-
-
-  return <>
-  <ul>{res}</ul></>
-
+return <>
+{res}</>
+  
 }
+
 export default App;
