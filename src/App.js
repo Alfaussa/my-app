8c187f360ef8d06e00b1c33040e2ffd1d5a8a5c3
Alfiya <al.yausheva@gmail.com>
2025-02-19 10:22:50 +0300

    ⊗jsrtPmFmsDt 5

--- src/App.js
+++ src/App.js
@@ -9,43 +9,27 @@ import { nanoid } from 'nanoid'
 
 
 function App() {
-  let options = {year: "numeric", month:"numeric",day:"numeric"}
-let curDate = new Date().toLocaleDateString("en-US", options);
-let arr = curDate.split('/');
-let num = +arr[0];
-function padTo2Digits(num) {
-    
-  return num.toString().padStart(2, '0'); // Преобразует 9 в 09, а 10 оставит без изменений
-     }
-  
-    let month = padTo2Digits(num);
-    
-  
-    let res = arr[2] + '-' + month+ '-' + arr[1];
+
+const [num, numSet] = useState();
+const [result, setResult] = useState(0)
 
 
-const [date1, setDate1] = useState(res);
-const [date2, setDate2] = useState(res);
-const [result, setResult] = useState(0);
+
+function digitSum(){
+  const arr = Array.from(String(num)).map(string => +string)
+  const sum = arr.reduce((acc, cur) => (acc + cur));
+  setResult(sum)
+}    
 
 
-    function dateDiff(){
 
-      setResult(Math.floor(new Date(date2) - new Date(date1)) / (1000*60*60*24));
-    }
-  
-
-    
-    
-    
-    
-    
-
+ 
 return <>
 
-<input type='date' id = 'date1' value = {date1} onChange={event =>setDate1(event.target.value)}></input>
-<input type='date' id = 'date2' value = {date2} onChange={event => setDate2(event.target.value)}></input>
-<button onClick={dateDiff}></button>
+<input value = {num} onChange={event =>numSet(event.target.value)}
+ onBlur={digitSum}></input>
+
+
 <p>result: {result} </p>
 </>
 
