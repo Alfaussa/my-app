f6dc48fa4912383a0a37c1b6944a431b5d26b775
Alfiya <al.yausheva@gmail.com>
2025-02-19 16:40:05 +0300

    ⊗jsrtPmFmsDt 4

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // ⊗jsrtPmFmsDt 
 // 
 // 
-// №2
+// №4
 
 import React, {useState} from 'react';
 import { nanoid } from 'nanoid'
@@ -15,19 +15,25 @@ const [result, setResult] = useState(0)
 
 
 
-function digitSum(){
-  const arr = Array.from(String(num)).map(string => +string)
-  const sum = arr.reduce((acc, cur) => (acc + cur));
-  setResult(sum)
-}    
+function multiDivisors(){
+  let arr = [];
+  for (let i = 1; i <= num; i++ ){
+    if(num % i === 0)
+      arr.push(i)
+  } 
+  const res = arr.reduce((acc, cur) => (acc * cur))
 
+  setResult(res)
+  
+
+}
 
 
  
 return <>
 
 <input value = {num} onChange={event =>numSet(event.target.value)}
- onBlur={digitSum}></input>
+ onBlur={multiDivisors}></input>
 
 
 <p>result: {result} </p>
