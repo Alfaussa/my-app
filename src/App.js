6c72bbf6d7bff29692283ef9e3bccb0ffee78b9c
Alfiya <al.yausheva@gmail.com>
2025-02-21 21:50:51 +0300

    ⊗jsrtPmFmsChI 1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-// ⊗jsrtPmFmsTx 
+⊗jsrtPmFmsChI
 // 
 // 
 // №1
@@ -9,21 +9,18 @@ import { nanoid } from 'nanoid'
 
 
 function App() {
-const [checked1, setChecked1] = useState(true);
-const [checked2, setChecked2] = useState(true);
-const [checked3, setChecked3] = useState(true);
+const [checked, setChecked] = useState(true);
+const [result, setResult] = useState();
 
+function showMessage(event){
+ setResult(checked ? 'hello, user! ' : 'bye, user!')
+}
 
  
 return <>
-<label><input type = 'checkbox' checked = {checked1} onChange={() => setChecked1(!checked1)}/>html</label>
-<label><input type = 'checkbox' checked = {checked2} onChange={() => setChecked2(!checked2)}/>css</label>
-<label><input type = 'checkbox' checked = {checked3} onChange={() => setChecked3(!checked3)}/>js</label>
-<p>Вы выбрали:</p>
-<p>{checked1 ? 'html' : ''}</p>
-<p>{checked2 ? 'css' : ''}</p>
-<p>{checked3 ? 'js' : ''}</p>
-
+<input type = 'checkbox' checked = {checked} onChange={() => setChecked(!checked)}/>
+<button onClick={showMessage }></button>
+<p>{result}</p>
 
 </>
 }
