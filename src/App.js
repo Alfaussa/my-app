d03c4991311896bcdef2fd676f9802ea1583fe0a
Alfiya <al.yausheva@gmail.com>
2025-03-02 21:57:09 +0300

    ⊗jsrtPmDtOAA 2

--- src/App.js
+++ src/App.js
@@ -1,43 +1,71 @@
-⊗jsrtPmDtAOp №2//
+//⊗jsrtPmDtOAA №2
 
 import React, {useState} from 'react';
 import { nanoid } from 'nanoid'
 
 function App() {
 
-  const [notes, setNotes] = useState([1, 2, 3, 4, 5]);
+  const initNotes = [
+    {
+      id: 'GYi9G_uC4gBF1e2SixDvu',
+      prop1: 'value11',
+      prop2: 'value12',
+      prop3: 'value13',
+    },
+    {
+      id: 'IWSpfBPSV3SXgRF87uO74',
+      prop1: 'value21',
+      prop2: 'value22',
+      prop3: 'value23',
+    },
+    {
+      id: 'JAmjRlfQT8rLTm5tG2m1L',
+      prop1: 'value31',
+      prop2: 'value32',
+      prop3: 'value33',
+    },
+  ];
 
 
-  const result = notes.map((note, index) => {
+  const [notes, setNotes] = useState(initNotes);
+  const [value1, setValue1] = useState('');
+  const [value2, setValue2] = useState('');
+  const [value3, setValue3] = useState('');
+  
+  const result = notes.map((note) => {
     return <div> 
-      <li key = {nanoid()} onClick={() => squaredNum(index)}> {note} </li>
-      <button onClick={() => deleteLi(index)}>delete</button>
+
+      <li key = {note.id} > 
+       <span>{note.prop1} </span> 
+       <span>{note.prop2} </span> 
+       <span>{note.prop3} </span> 
+         </li>
       </div>
   })
-  let copy = Object.assign([], notes);
-  function squaredNum(index) {
-    copy[index] = copy[index] ** 2  ;
-    setNotes(copy);
-  }
-  
-  function deleteLi(index){
-  let copy1 = Object.assign([], notes);
-  copy1.splice(index, 1);
-  setNotes(copy1);
-  }
+  const newObj = new Object();
+  newObj.id = nanoid();
+  newObj.prop1 = value1;
+  newObj.prop2 = value2;
+  newObj.prop3 = value3;
 
-  return <>
   
+ function addLi(){
+  let copy = Object.assign([], initNotes);
+  copy.push(newObj);
+  
+  setNotes(copy)
+ }
+  
+  return <>
+  <input onChange={(event) => setValue1(event.target.value)}></input>
+  <input onChange={(event) => setValue2(event.target.value)}></input>
+  <input onChange={(event) => setValue3(event.target.value)}></input>
   <ul>
     {result}
   </ul>
+  <button onClick={() => addLi()}></button>
   </>
   
   }
   
   export default App;
-
-
-
-
-
