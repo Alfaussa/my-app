75bab93ae2d4cdf82fce4d770413da7065156274
Alfiya <al.yausheva@gmail.com>
2025-03-03 11:25:45 +0300

    +

--- src/App.js
+++ src/App.js
@@ -1,44 +1,60 @@
-//⊗jsrtPmDtAOp №4
+//⊗jsrtPmDtOAA №1
+
 
 import React, {useState} from 'react';
 import { nanoid } from 'nanoid'
 
 function App() {
 
-  const [notes, setNotes] = useState(["hi", "my", "name", "is", "Alya"]);
-  const [value, setValue] = useState();
-  const [index, setIndex] = useState();
+  const initNotes = [
+    {
+      id: 'GYi9G_uC4gBF1e2SixDvu',
+      prop1: 'value11',
+      prop2: 'value12',
+      prop3: 'value13',
+    },
+    {
+      id: 'IWSpfBPSV3SXgRF87uO74',
+      prop1: 'value21',
+      prop2: 'value22',
+      prop3: 'value23',
+    },
+    {
+      id: 'JAmjRlfQT8rLTm5tG2m1L',
+      prop1: 'value31',
+      prop2: 'value32',
+      prop3: 'value33',
+    },
+  ];
+
+  const [notes, setNotes] = useState(initNotes);
 
 
-
-  const result = notes.map((note, index) => {
-    return <div> 
-      <li key = {nanoid()} onClick={() => textTransfer(index)}> {note} </li>
-      </div>
+  const result = notes.map((note) => {
+    return  <li key = {note.id} > 
+       <span>{note.prop1} </span> 
+       <span>{note.prop2} </span> 
+       <span>{note.prop3} </span> 
+         </li>
+      
   })
+
+  const obj = {id: nanoid(), prop1:'new', prop2:'new', prop3:'new'}
   
-function  textTransfer(index){
-let res = notes[index];
-setValue(res); 
-setIndex(index);
-}
-
-
-function returnText(index){
+ function addLi(){
   let copy = Object.assign([], notes);
-
-  copy[index]=value;
+  let obj = {id: nanoid(), prop1:'new', prop2:'new', prop3:'new'}
+  copy.push(obj);
   setNotes(copy)
-}
-
+ }
+  
 
   return <>
   
   <ul>
     {result}
-  
   </ul>
-  <input value = {value} onChange={(event) => setValue(event.target.value)} onBlur={() => returnText(index)}></input>
+  <button onClick={() => addLi()}></button>
   </>
   
   }
