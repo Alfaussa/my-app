5fca77fab044386037ab078a57f3b2df8640ef56
Alfiya <al.yausheva@gmail.com>
2025-04-17 11:12:56 +0300

    ⊗jsrtPmCpChA

--- src/App.js
+++ src/App.js
@@ -8,15 +8,18 @@ import Employee from './Employee';
 import { nanoid } from 'nanoid'
 
 function App() {
-  const personalData = [
-    {id: nanoid(), name: 'Alex', surname: 'Fedor', age: 18},
-    {id: nanoid(), name: 'Alexey', surname: 'Fedorov', age: 28},
-    {id: nanoid(), name: 'Alexandr', surname: 'Fedorenskiy', age: 38},
+  const users = [
+    {id: nanoid(), name: 'user1', surn: 'surn1', 
+      age: 30}, 
+    {id: nanoid(), name: 'user2', surn: 'surn2', 
+      age: 31}, 
+    {id: nanoid(), name: 'user3', surn: 'surn3', 
+      age: 32}, 
   ];
 return <>
- 	<User name={personalData[0].name} surname={personalData[0].surname} age={personalData[0].age}/>
-   <User name={personalData[1].name} surname={personalData[1].surname} age={personalData[1].age}/>
-   <User name={personalData[2].name} surname={personalData[2].surname} age={personalData[2].age}/>
+ 	<User name={users[0].name} surname={users[0].surn} age={users[0].age}/>
+   <User name={users[1].name} surname={users[1].surn} age={users[1].age}/>
+   <User name={users[2].name} surname={users[2].surn} age={users[2].age}/>
  </>
   
   }
