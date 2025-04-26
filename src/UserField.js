565eb157ae6f96152487bcc71cfbf758ad2a4eee
Alfiya <al.yausheva@gmail.com>
2025-04-26 23:01:12 +0300

    ⊗jsrtPmCpEGS1

--- src/UserField.js
+++ src/UserField.js
@@ -1,15 +1,14 @@
 import React from "react";
 
-function User({name, surname, age, id, isDataEdit, toggleUser, editUser}){
+function UserField({text, type, id, isDataEdit, editUser}){
     return <>
     <tr>
-        <td>Имя: 
+        <td>
             {isDataEdit 
-            ? <input value ={name} 
+            ? <input value ={text} 
             onChange={(event) => editUser(id, 'name', event)}/> : <span>{name}</span> }</td>
         <td>Фамилия: {isDataEdit ? <input value ={surname} onChange={(event) => editUser(id, 'surn', event)}/> : <span>{surname}</span>}</td>
         <td>Возраст: {isDataEdit ? <input value ={age} onChange={(event) => editUser(id, 'age', event)}/> : <span>{age}</span>}</td>
-       <button onClick={() => toggleUser(id)}>{isDataEdit ? 'сохранить' : 'редактировать' }</button>
     </tr>
     </>
 }
