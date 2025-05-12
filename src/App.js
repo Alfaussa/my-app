4770d427c6935306c72ccc46393f9963b72ead85
Alfiya <al.yausheva@gmail.com>
2025-05-12 22:55:59 +0300

    ⊗jsrtPmSyCFTS

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Employee from './Employee';
 import { nanoid } from 'nanoid'
 import Users from './Users';
 import Calculator from './Calculator';
+import {styles} from './styles.js'
 
 
 
@@ -15,32 +16,14 @@ import Calculator from './Calculator';
 function App() {
 
 
-	const class1 = {
-		width: '200px',
-		border: '2px solid brown',
-		padding: '10px',
-		textAlign: 'center'
-	};
-	const class2 = {
-		color: 'orangered',
-		fontWeight: 'bold'
-	}
-	const class3 = {
-		fontStyle: 'italic',
-		color: 'brown',
-	}
-	const class4 = {
-		backgroundColor: 'orange',
-		fontWeight: 'bold',
-		color: 'white',
-	}		
+	
 return  <>
 
 
-<div style={class1}>
-			<p style={class2}>text</p>
-			<p style={class3}>text</p>
-			<p style={class4}>text</p>
+<div style={styles.class1}>
+			<p style={styles.class2}>text</p>
+			<p style={styles.class3}>text</p>
+			<p style={styles.class4}>text</p>
 		</div>
 <Calculator/>
 
