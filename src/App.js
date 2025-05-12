e6ce4e5965f1bf58c08e09fbf7c4fcfad6dc53cb
Alfiya <al.yausheva@gmail.com>
2025-05-12 17:39:30 +0300

    inline styles

--- src/App.js
+++ src/App.js
@@ -8,19 +8,39 @@ import Employee from './Employee';
 import { nanoid } from 'nanoid'
 import Users from './Users';
 import Calculator from './Calculator';
-import './'
+
 
 
 
 function App() {
 
+
+	const class1 = {
+		width: '200px',
+		border: '2px solid brown',
+		padding: '10px',
+		textAlign: 'center'
+	};
+	const class2 = {
+		color: 'orangered',
+		fontWeight: 'bold'
+	}
+	const class3 = {
+		fontStyle: 'italic',
+		color: 'brown',
+	}
+	const class4 = {
+		backgroundColor: 'orange',
+		fontWeight: 'bold',
+		color: 'white',
+	}		
 return  <>
 
 
-<div>
-			<p>text</p>
-			<p>text</p>
-			<p>text</p>
+<div style={class1}>
+			<p style={class2}>text</p>
+			<p style={class3}>text</p>
+			<p style={class4}>text</p>
 		</div>
 
 </>
