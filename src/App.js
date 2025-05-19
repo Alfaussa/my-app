8b940c9263761fb7168c4167f4b6b5166456fd77
Alfiya <al.yausheva@gmail.com>
2025-05-19 22:22:04 +0300

    styles in const

--- src/App.js
+++ src/App.js
@@ -15,20 +15,39 @@ import Calculator from './Calculator';
 
 function App() {
 
+const wd1 = '200px';
+const br1 = '2px solid brown';
+const pd1 = '10px';
+const ta1 = 'center';
+const co1 = 'orangered';
+const fw1 = 'bold';
+const fs1 = 'italic';
+const co2 = 'brown';
+const bco1 = 'orange';
+const co3 = 'white';
 
-	
 return  <>
 
 
-<div >
-			<p style = {{
-				color: 'orangered',
-				fontWeight: 'bold'}}>
-					text</p>
-			<p style = {{
-				color: 'red'
-			}}>text</p>
-			<p >text</p>
+<div style = {{
+			width: wd1,
+			border: br1,
+			padding: pd1,
+			textAlign: ta1 }}>
+			<p style = {{ color: co1, fontWeight: 
+				fw1 }}> 
+				text
+			</p>
+			<p style = {{ fontStyle: fs1, color: 
+				co2 }}> 
+				text
+			</p>
+			<p style = {{ 
+				backgroundColor: bco1,
+				fontWeight: fw1,
+				color: co3 }}>
+				text
+			</p>
 		</div>
 <Calculator/>
 
