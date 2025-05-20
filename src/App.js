c640f9fa38fac778081e12b6527b21bae8034aa1
Alfiya <al.yausheva@gmail.com>
2025-05-20 09:49:29 +0300

    props

--- src/App.js
+++ src/App.js
@@ -11,27 +11,17 @@ import Calculator from './Calculator';
 //import {styles} from './styles.js'
 import styled from 'styled-components';
 
-const Text1 = styled.p`
-font-style: italic;
-color: brown;
-`;
-const Text2 = styled.p`
-	font-style: italic;
-	color: brown;
-`;
-
-const Text3 = styled.p`
-	background-color: orange;
-	font-weight: bold;
-	color: white;
-`;
-
 const Container = styled.div`
-	width: 200px;
-	border: 2px solid brown;
-	padding: 10px;
-	text-align: center;
+	display: flex;
+	flex-direction: column;
+	width: 150px;
 `;
+
+const Input = styled.input`
+	margin: 5px;
+	font-size: 18px;
+`;
+
 function App() {
 
 
@@ -39,12 +29,13 @@ function App() {
 return  <>
 
 
-<Container>
-	<Text1>text</Text1>
-	<Text2>text</Text2>
-	<Text3>tetx</Text3>
-</Container>
-<Calculator/>
+
+		<Container>
+			<Input />
+			<Input placeholder = "name" type = "text" />
+			<Input type = "password"/>
+		</Container>
+	
 
 </>
 
